16712983b5d0b451f04a179b755c79d3e533e8fe
FarrosAtsir <ayanokouji120108@gmail.com>
2024-12-09 14:30:37 +0700

    hahaha

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -97,14 +97,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "603dd73423079d63ba01f9342022be64
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "023093a2ecf38a9a6c2fd1839dd71131",
+"flutter_bootstrap.js": "01157376ea99460090b5ba13226ddc1a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a92e817726f56479387534abfbb4d984",
 "/": "a92e817726f56479387534abfbb4d984",
-"main.dart.js": "bdb8641b886e7a2164323c1bb1d2683e",
+"main.dart.js": "4407e1e34a285c8e2c0e2934d640e83b",
 "manifest.json": "337fbd5671357c8c166d74e98a8397da",
 "version.json": "882561a23e46efb349f16f4735e1e14a"};
 // The application shell files that are downloaded before a service worker can
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
